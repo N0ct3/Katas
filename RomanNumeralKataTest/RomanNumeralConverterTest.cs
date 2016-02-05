55d79c280fe7b04b3ce70845c9a9ba6f78969934
Nocte <antoine.barbier@live.com>
2016-02-05 23:30:03 +0100

    Added some tests for basic values

---
+++ RomanNumeralKataTest/RomanNumeralConverterTest.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using RomanNumeralKata;
+using FluentAssertions;
+
+namespace RomanNumeralKataTest
+{
+    public class RomanNumeralConverterTest
+    {
+        [Fact]
+        public void One_Should_Return_I()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var one = converter.Convert(1);
+
+            one.Should().Be("I");
+        }
+
+        [Fact]
+        public void Five_Should_Return_V()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var five = converter.Convert(5);
+
+            five.Should().Be("V");
+        }
+
+        [Fact]
+        public void Ten_Should_Return_X()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var ten = converter.Convert(10);
+
+            ten.Should().Be("X");
+        }
+
+        [Fact]
+        public void Two_Should_Return_II()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var two = converter.Convert(2);
+
+            two.Should().Be("II");
+        }
+
+        [Fact]
+        public void Three_Should_Return_III()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var three = converter.Convert(3);
+
+            three.Should().Be("III");
+        }
+
+        [Fact]
+        public void Four_Should_Return_IV()
+        {
+            var converter = new RomanNumeralConverter();
+
+            var four = converter.Convert(4);
+
+            four.Should().Be("IV");
+        }
+
+        [Theory]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        public void Int_Should_Return_String(int numberToConvert, string expectedResult)
+        {
+            var converter = new RomanNumeralConverter();
+
+            var actual = converter.Convert(numberToConvert);
+
+            actual.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ RomanNumeralKataTests1/RomanNumeralConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using RomanNumeralKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralKata.Tests
+{
+    public class RomanNumeralConverterTests
+    {
+        [Fact()]
+        public void ConvertTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
