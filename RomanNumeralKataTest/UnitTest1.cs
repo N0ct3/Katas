f5c1b2cc35f8620a0da0bfcf57ae902998005bc8
Nocte <antoine.barbier@live.com>
2016-02-05 22:41:53 +0100

    Initial Commit

---
+++ RomanNumeralKataTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumeralKataTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
