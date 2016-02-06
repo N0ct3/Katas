59a1225f9b37e56dd2a0614c3c03f7c6fd2d414b
Nocte <antoine.barbier@live.com>
2016-02-07 00:50:22 +0100

    Added new StringCalculator kata

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Sum(string intToSum)
+        {
+            if (string.IsNullOrEmpty(intToSum))
+            {
+                return 0;
+            }
+
+            var delimiters = new List<string> { ",", "\n" };
+            if (DeclaresNewDelimiter(intToSum))
+            {
+                UpdateDelimitersList(intToSum, delimiters);
+                intToSum = GetStringWithoutNewDelimiter(intToSum);
+            }
+
+            var splitValues = intToSum.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(stringValue => int.Parse(stringValue));
+            CheckForNegativeValues(splitValues);
+
+            return splitValues.Where(value => value < 1000).Aggregate((currentSum, nextValue) => currentSum + nextValue);
+        }
+
+        private bool DeclaresNewDelimiter(string value)
+        {
+            return value.StartsWith(@"\", StringComparison.Ordinal);
+        }
+
+        private void CheckForNegativeValues(IEnumerable<int> splitValues)
+        {
+            var negativeValues = splitValues.Where(value => value < 0).ToList();
+            if (negativeValues.Any())
+            {
+                string preparedString = string.Empty;
+                negativeValues.ForEach(currentValue => preparedString += preparedString.Any() ? (", " + currentValue.ToString()) : currentValue.ToString());
+                throw new Exception(string.Format("Negatives not allowed: {0}", preparedString));
+            }
+        }
+
+        private string GetStringWithoutNewDelimiter(string intToSum)
+        {
+            return intToSum.Substring(intToSum.IndexOf('\n') + 1);
+        }
+
+        private void UpdateDelimitersList(string intToSum, List<string> delimiters)
+        {
+            if (intToSum.Contains("["))
+            {
+                var regex = new Regex(@"(?<=\[)(.*?)(?=\])");
+                var newDelimiters = regex.Matches(intToSum);
+                foreach (var newDelimiter in newDelimiters)
+                {
+                    delimiters.Add(newDelimiter.ToString());
+                }
+            }
+            else
+            {
+                delimiters.Add(intToSum[1].ToString());
+            }
+
+        }
+    }
+}
---
+++ StringCalculatorKataTests/StringCalculatorTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void StringNumberShouldReturnInt(string number, int expectedValue)
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actualValue = stringCalculator.Sum(number);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void OneAndTwoShouldReturn3()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int three = stringCalculator.Sum("1,2");
+
+            three.Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("1\n2, 3", 6)]
+        [InlineData("10,51,301", 362)]
+        [InlineData("0,0,0", 0)]
+        public void SeveralStringNumbersShouldReturnSummetValue(string number, int expectedValue)
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actualValue = stringCalculator.Sum(number);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void NewLineShouldWorkAsComma()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int three = stringCalculator.Sum("1\n2");
+
+            three.Should().Be(3);
+        }
+
+        [Fact]
+        public void DelimiterShouldBeChangeable()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int three = stringCalculator.Sum("\\;\n1;2");
+
+            three.Should().Be(3);
+        }
+
+        [Fact]
+        public void NegativeValueShouldThrowException()
+        {
+            var stringCalculator = new StringCalculator();
+            Action action = () => stringCalculator.Sum("-1");
+
+            action.ShouldThrow<Exception>()
+                                .WithMessage("Negatives not allowed*");
+        }
+
+        [Fact]
+        public void NegativeValuesShouldThrowException()
+        {
+            var stringCalculator = new StringCalculator();
+            Action action = () => stringCalculator.Sum("-1,-2,5");
+
+            action.ShouldThrow<Exception>()
+                                .WithMessage("Negatives not allowed: -1, -2");
+        }
+
+        [Fact]
+        public void NumberGreaterThan1000ShouldBeIgnored()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int five = stringCalculator.Sum("1000,5");
+
+            five.Should().Be(5);
+        }
+
+        [Theory]
+        [InlineData("\\[xxxxxxx]\n1xxxxxxx2xxxxxxx3", 6)]
+        [InlineData("\\[%%]\n1%%2%%3", 6)]
+        [InlineData("\\[!!!!]\n1!!!!2!!!!3", 6)]
+        public void DelimiterOfAnyLengthShouldBeHandled(string number, int expectedValue)
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actualValue = stringCalculator.Sum(number);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("\\[***][%%%]\n1***2%%%3", 6)]
+        [InlineData("\\[*][%]\n1*2%3", 6)]
+        [InlineData("\\[!!!!][xxxxxxx]\n1xxxxxxx2!!!!3", 6)]
+        public void MultipleNewDelimitersShouldBeHandled(string number, int expectedValue)
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actualValue = stringCalculator.Sum(number);
+
+            actualValue.Should().Be(expectedValue);
+        }
+
+    }
+}
